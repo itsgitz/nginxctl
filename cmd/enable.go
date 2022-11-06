3f942f6308323bb0e277503ce25dcdd26106050b
itsgitz <anggit.ginanjar@outlook.com>
2022-11-06 09:51:40 +0700

    feat(cmd): Added disable and enable command for vhost

---
+++ cmd/enable.go
@@ -0,0 +1,53 @@
+/*
+Copyright © 2022 Anggit M Ginanjar <anggit.ginanjar.dev@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// enableCmd represents the enable command
+var enableCmd = &cobra.Command{
+	Use:   "enable",
+	Short: "Enable a virtualhost",
+	Long: `Enable an available virtualhost by create configuration link
+from "/etc/nginx/sites-available" to "/etc/nginx/sites-enabled"`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("enable called")
+	},
+}
+
+func init() {
+	vhostCmd.AddCommand(enableCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// enableCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// enableCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
